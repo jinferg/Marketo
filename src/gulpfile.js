9d76526dfea7358d5f2ffbc2b7403f430b0ec35b
Jin Su Park <jinsupark@icloud.com>
2018-06-26 12:50:33 +1200

    U

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -7,7 +7,6 @@ const mqpacker = require('css-mqpacker'); // consolidate media query
 const cssnano = require('cssnano'); // minify css
 const concat = require('gulp-concat'); // concatenate js
 const uglify = require('gulp-uglify'); // minify js
-const imagemin = require('gulp-imagemin'); // minify images
 
 /*
 Top Level Functions
@@ -23,17 +22,12 @@ gulp.task('css', function () {
   console.log('gulp css started')
   var plugins = [
     cssnext({ browsers: ["last 2 versions"] }),
-    // cssnext({ features: {
-    //             autoprefixer: false
-    //           }
-    //         }),
     mqpacker(),
     cssnano({ autoprefixer: false })
   ];
-  return gulp.src('styles/main.scss')
+  return gulp.src('css/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
-    // .pipe(autoprefixer({ grid: true, browsers: ["last 2 major versions", "not ie < 11"] }))
     .pipe(gulp.dest('../static/assets/css'))
 });
 
@@ -50,20 +44,20 @@ gulp.task('js', function () {
                    'vendors/prism/prism.js',
                    'vendors/scrollreveal/scrollreveal.min.js',
                    'vendors/typed/typed.min.js',
-                   'scripts/components/tab.js',
-                   'scripts/components/accordion.js',
-                   'scripts/components/dropdown.js',
-                   'scripts/components/slidemenu.js',
-                   'scripts/components/notice.js',
-                   'scripts/main.js'])
+                   'js/components/tab.js',
+                   'js/components/accordion.js',
+                   'js/components/dropdown.js',
+                   'js/components/slidemenu.js',
+                   'js/components/notice.js',
+                   'js/main.js'])
 		.pipe(concat('main.js'))
 		.pipe(gulp.dest('../static/assets/js'))
 });
 
 // Watch
 gulp.task('watch', ['default'], function () {
-  gulp.watch(['styles/**/*.*'], ['css'])
-  gulp.watch(['scripts/**/*.*'], ['js'])
+  gulp.watch(['css/**/*.*'], ['css'])
+  gulp.watch(['js/**/*.*'], ['js'])
   gulp.watch(['vendor/**/*.*'], ['js'])
 });
 
